62c1ee5e944af595980a373a0d2bf6a7b6c9e627
Timir <timirgantait000@gmail.com>
2024-05-09 00:34:56 +0530

    add signup and errorhandling logic

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { log } from 'console';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
@@ -13,7 +13,20 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 501;
+    const message = err.message || "Internal Server Error!"//message is default of javascript but statusCode is manual
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
