66e0c8f318af0e57f5780bd688e7ebe5e624bbc3
Timir <timirgantait000@gmail.com>
2024-05-09 01:11:35 +0530

    add signup ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold py-7 text-center'>Sign Up</h1>  
+
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg' />
+
+        <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg' />
+
+        <input type="password" placeholder='password' id='password' className='border p-3 rounded-lg' />
+      </form>         
+    </div>
   )
 }
 
